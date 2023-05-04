9e39d74141db88dd8cb466843fba08ada72620de
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-05-04 19:51:49 +0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+require_once '..\vendor\autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__.'/app/Views');
+$twig = new Environment($loader);
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/basic', [RickAndMorty\Controllers\CharactersController::class, 'allCharacters']);
+    $r->addRoute('GET', '/search', [RickAndMorty\Controllers\CharactersController::class, 'oneCharacter']);
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        [$controllerName, $methodName] = $handler;
+
+        $controller = new $controllerName;
+
+        /** @var \RickAndMorty\View $response */
+
+        $response = $controller->{$methodName}();
+
+        echo $twig->render($response->getTemplate() . '.html.twig', $response->getCollection());
+
+        break;
+}
