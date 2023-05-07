fe7209c83301a60feaedc9d706b122f122519b77
ArtjomsPra <75216514+ArtjomsPra@users.noreply.github.com>
2023-05-07 14:28:56 +0300

    Update Character.php

--- app/Model/Character.php
+++ app/Model/Character.php
@@ -5,57 +5,57 @@ namespace RickAndMorty\Model;
 class Character
 {
 
-private string $image;
-private string $name;
-private string $status;
-private string $species;
-private string $location;
-private string $episode;
+    private string $image;
+    private string $name;
+    private string $status;
+    private string $species;
+    private string $location;
+    private string $episode;
 
-public function __construct(
-    string $image,
-    string $name,
-    string $status,
-    string $species,
-    string $location,
-    string $episode)
-{
+    public function __construct(
+        string $image,
+        string $name,
+        string $status,
+        string $species,
+        string $location,
+        string $episode)
+    {
 
-    $this->image = $image;
-    $this->name = $name;
-    $this->status = $status;
-    $this->species = $species;
-    $this->location = $location;
-    $this->episode = $episode;
+        $this->image = $image;
+        $this->name = $name;
+        $this->status = $status;
+        $this->species = $species;
+        $this->location = $location;
+        $this->episode = $episode;
 
-}
+    }
 
-    public function getImage() : string
+    public function getImage(): string
     {
         return $this->image;
     }
 
-    public function getName() : string
+    public function getName(): string
     {
         return $this->name;
     }
 
-    public function getStatus() : string
+    public function getStatus(): string
     {
         return $this->status;
     }
 
-    public function getSpecies() : string
+    public function getSpecies(): string
     {
         return $this->species;
     }
 
-    public function getLocation() : string
+    public function getLocation(): string
     {
         return $this->location;
     }
 
-    public function getEpisode() : string
+    public function getEpisode(): string
     {
         return $this->episode;
     }
